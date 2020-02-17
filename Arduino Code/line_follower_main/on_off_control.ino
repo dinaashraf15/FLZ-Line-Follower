147c5742321403461501f32e7edf1c3cda77b3a5
dinaashraf15 <61041337+dinaashraf15@users.noreply.github.com>
2020-02-16 20:58:25 -0800

    Update on_off_control.ino

--- Arduino Code/line_follower_main/on_off_control.ino
+++ Arduino Code/line_follower_main/on_off_control.ino
@@ -1,4 +1,16 @@
+
 void controlOnOff(int error){
-  
+if (error == (00001)&&error == (00010)){
+  moveR();
+}
+else if (error == (10000)&&error == (01000)){
+  moveL();
+}
+else if (error == (00000)){
+  moveF();
+}
+else if (error == (00100)){
+  stopropot();
 }
 
+}
